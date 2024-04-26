b5e7ec63cd5832a4a230c2322f8f4dbbe56eca4a
Nicolas_Notebook <gamboa.nicolas2002@gmail.com>
2024-04-26 17:55:12 -0300

    Metrópolis Hasting en 2D. Creación de plot_hotmap (Ver título de leyenda)

--- Funciones.R
+++ Funciones.R
@@ -25,3 +25,33 @@ plot_autocor <- function(muestra) {
     geom_hline(yintercept = 0, linewidth = 0.9) +
     labs(x = "Rezago", y = "Autocorrelación")
 }
+
+plot_hotmap <- function(muestra, d_objetivo, puntos = T) {
+  
+  if (ncol(muestra) != (2+1)) {
+    
+  }
+  
+  df_grilla <- expand.grid(x = seq(min(muestra$dim_1),max(muestra$dim_1), length.out = 200),
+                           y = seq(min(muestra$dim_2),max(muestra$dim_2), length.out = 200))
+  df_grilla$z <- d_objetivo(as.matrix(df_grilla))
+  
+  hotmap <- muestra |> 
+    ggplot(aes(x = dim_1, y = dim_2)) +
+    geom_density2d_filled() +
+    stat_contour(aes(x = x, y = y, z = z), data = df_grilla, col = "white", alpha = 0.5) +
+    # labs(subtitle = "En blanco la distribución objetivo")
+    theme(legend.title = "Densidad")
+  if (puntos) {
+    hotmap +
+      geom_point(color = "black", 
+                 fill = "firebrick2", 
+                 size = 2, pch = 21, 
+                 alpha = 0.3)
+  } else {
+    return(hotmap)
+  }
+}
+
+
+
